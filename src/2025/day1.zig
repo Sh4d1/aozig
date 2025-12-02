6dcd450f884fd22e731d21175293c1a15db4c524
Patrik Cyvoct <patrik@ptrk.io>
2025-12-02 01:39:04 +0100

    day1
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

--- src/2025/day1.zig
+++ src/2025/day1.zig
@@ -1,28 +1,95 @@
 const std = @import("std");
 const aozig = @import("aozig");
 
-pub var alloc: std.mem.Allocator = undefined;
+pub var alloc: std.mem.Allocator = std.heap.page_allocator;
 
-pub fn parse(input: []const u8) !void {{
-    _ = input;
-    // TODO: Parse input
-}}
+const Direction = enum {
+    Left,
+    Right,
+};
 
-pub fn solve1(_: void) usize {{
-    // TODO: Solve part 1
-    return 0;
-}}
+const Rotation = struct {
+    direction: Direction,
+    distance: isize,
+};
 
-pub fn solve2(_: void) usize {{
-    // TODO: Solve part 2
-    return 0;
-}}
+const Dial = struct {
+    position: isize = 50,
 
-test "example" {{
-    const input =
-        \\
-    ;
-    _ = try parse(input);
-    try std.testing.expectEqual(@as(usize, 0), solve1({{}}));
-    try std.testing.expectEqual(@as(usize, 0), solve2({{}}));
-}}
+    fn rotate(self: *Dial, rotation: *const Rotation) isize {
+        const distance = @mod(rotation.distance, 100);
+        var res = @divFloor(rotation.distance, 100);
+        switch (rotation.direction) {
+            .Left => {
+                if (self.position != 0 and self.position < distance) res += 1;
+                self.position = @mod(self.position - distance, 100);
+            },
+            .Right => {
+                if (100 - self.position < distance) res += 1;
+                self.position = @mod(self.position + distance, 100);
+            },
+        }
+        return res;
+    }
+};
+
+pub fn parse(input: []const u8) ![]Rotation {
+    var lines = std.mem.splitAny(u8, input, "\n");
+    var rotations: std.array_list.Aligned(Rotation, null) = .empty;
+    defer rotations.deinit(alloc);
+    while (lines.next()) |line| {
+        if (line.len == 0) continue;
+        switch (line[0]) {
+            'L' => try rotations.append(alloc, Rotation{ .direction = Direction.Left, .distance = try std.fmt.parseInt(isize, line[1..], 10) }),
+            'R' => try rotations.append(alloc, Rotation{ .direction = Direction.Right, .distance = try std.fmt.parseInt(isize, line[1..], 10) }),
+            else => return error.InvalidInput,
+        }
+    }
+    return rotations.toOwnedSlice(alloc);
+}
+
+pub fn solve1(rotations: []Rotation) usize {
+    var dial = Dial{};
+    var res: usize = 0;
+
+    for (rotations) |rotation| {
+        _ = dial.rotate(&rotation);
+        if (dial.position == 0) {
+            res += 1;
+        }
+    }
+    return res;
+}
+
+pub fn solve2(rotations: []Rotation) isize {
+    var dial = Dial{};
+    var res: isize = 0;
+
+    for (rotations) |rotation| {
+        res += dial.rotate(&rotation);
+        if (dial.position == 0) {
+            res += 1;
+        }
+    }
+    return res;
+}
+
+test "example" {
+    {
+        const input =
+            \\L68
+            \\L30
+            \\R48
+            \\L5
+            \\R60
+            \\L55
+            \\L1
+            \\L99
+            \\R14
+            \\L82
+        ;
+        const res = try parse(input);
+        try std.testing.expectEqual(@as(usize, 3), solve1(res));
+        try std.testing.expectEqual(@as(isize, 6), solve2(res));
+    }
+}
